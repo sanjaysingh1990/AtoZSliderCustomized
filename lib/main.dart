0e967e9a03e5939a80390e1ade282ae13f07b68d
Sanjay Singh Bisht <sanjay.bisht@debutinfotech.com>
2019-07-12 17:02:25 +0530

    camera utils added for video

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:speech_bubble/speech_bubble.dart';
 
-void main() => runApp(MaterialApp(home: MyHomePage()));
+import 'camera_utils.dart';
+
+void main() => runApp(MaterialApp(home: CameraUtilsDemo()));
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
